d651e4476ce540b9f89589d1975cf5d4755add80
FredyR18 <fres628@hotmail.com>
2021-08-09 17:16:47 -0500

    Se agregan últimos talleres

---
+++ salarios.js
@@ -0,0 +1,82 @@
+const colombia = [];
+
+colombia.push({
+    name: "Juanita",
+    salario: 1000,
+});
+colombia.push({
+    name: "Pedro",
+    salario: 800,
+});
+colombia.push({
+    name: "Sergio",
+    salario: 1200,
+});
+colombia.push({
+    name: "Ana",
+    salario: 950,
+});
+colombia.push({
+    name: "Natalia",
+    salario: 700,
+});
+colombia.push({
+    name: "Pilar",
+    salario: 650,
+});
+colombia.push({
+    name: "Diego",
+    salario: 800,
+});
+colombia.push({
+    name: "Jose",
+    salario: 2000,
+});
+colombia.push({
+    name: "Sara",
+    salario: 1900,
+});
+colombia.push({
+    name: "Laura",
+    salario: 1500,
+});
+colombia.push({
+    name: "Javier",
+    salario: 1000,
+});
+colombia.push({
+    name: "Alex",
+    salario: 870,
+});
+colombia.push({
+    name: "Alberto",
+    salario: 1600,
+});
+colombia.push({
+    name: "Gustavo",
+    salario: 900,
+});
+colombia.push({
+    name: "Cristina",
+    salario: 1550,
+});
+colombia.push({
+    name: "Maria",
+    salario: 500,
+});
+colombia.push({
+    name: "Mario",
+    salario: 1400,
+});
+colombia.push({
+    name: "Jaime",
+    salario: 1300,
+});
+colombia.push({
+    name: "Diana",
+    salario: 1500,
+});
+colombia.push({
+    name: "Roberto",
+    salario: 1800,
+});
